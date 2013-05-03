1ca5708138611a591842008ed80c5d1fde148e00
Chris Xue <chrisxue815@gmail.com>
2013-05-03 15:44:55 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Minesweeper::Application.config.secret_token = 'b9502ec8fc125d64e613d3ee8ebdc74c89a5aac1122a55acdef6d8ea024607834858a587224878005977f89514d3ea4a27e16bec1b152ba1bf1257386fd4f3b1'

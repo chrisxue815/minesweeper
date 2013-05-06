193ae27db14d3fe7d576a80d311ca9023eda33ad
Chris Xue <chrisxue815@gmail.com>
2013-05-06 15:18:10 +0100

    add auto_open for openning blank grids automatically

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,7 +3,7 @@ class Game
   BoardHeight = 16
   NumMine = 40
 
-  attr_accessor :grids, :known_grids, :num_opened
+  attr_accessor :grids, :known_grids, :num_opened, :initial_grids
 
   def initialize(users)
     @grids = Hash.new(0)
@@ -40,14 +40,14 @@ class Game
       puts
     end
 
-    user_known_grids = open_blank(first_opened)
+    @initial_grids = open_blank(first_opened)
 
     @known_grids = Hash.new
     users.each_key do |name|
-      @known_grids[name] = user_known_grids.clone
+      @known_grids[name] = @initial_grids.clone
     end
 
-    @num_opened = Hash.new(user_known_grids.size)
+    @num_opened = Hash.new(@initial_grids.size)
   end
 
   def open(username, x, y)
@@ -55,19 +55,30 @@ class Game
     current = [x, y]
     grid = grids[current]
 
+    if grid.nil?
+      opened = {current => grid}
     if grid == 0
       opened = open_blank(x, y, user_known_grids)
-    else
-      opened = {current => grid}
+    elsif grid > 0
+      auto_open
     end
 
     opened.each do |position, value|
+      if value == :mine
+        mine_opened = true
+      end
+
       if user_known_grids[position]
         opened.delete(position)
       else
         user_known_grids[position] = value
-        num_opened[username] += 1
+        @num_opened[username] += 1
       end
+    end
+
+    if mine_opened
+      user_known_grids = @initial_grids.clone
+      @num_opened[username] = Hash.new(@initial_grids.size)
     end
 
     return opened
@@ -106,29 +117,43 @@ class Game
       x = x[0]
     end
 
+    current = [x, y]
     opened = Hash.new
-    pending = Array.new
+    pending = Array.new  # pending blank grids
 
-    pending.push([x, y])
+    opened[current] = grids[current]
+    pending.push(current)
 
     while pending.size > 0
       current = pending.delete_at(pending.size - 1)
-      opened[current] = grids[current]
 
       neighbors(current).each do |neighbor|
         next if opened[neighbor]
 
-        grid = grids[neighbor]
         known_grid = user_known_grids[neighbor]
+        next if known_grid == :marked
 
-        if grid != :mine && known_grid != :marked
-          opened[neighbor] = grid
-          pending.push(neighbor) if grid == 0
-        end
+        grid = grids[neighbor]
+        opened[neighbor] = grid
+
+        pending.push(neighbor) if grid == 0
       end
     end
 
     return opened
+  end
+
+  def auto_open(x, y = nil, user_known_grids = nil)
+    unless user_known_grids
+      user_known_grids = Hash.new
+    end
+
+    unless y
+      y = x[1]
+      x = x[0]
+    end
+
+
   end
 
   def neighbors(x, y = nil)

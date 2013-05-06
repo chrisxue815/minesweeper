78cef2534d112609dd166a0343f1414c81d1e3d2
Chris Xue <chrisxue815@gmail.com>
2013-05-06 21:17:57 +0100

    generate devise User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
-class User
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
+  # attr_accessible :title, :body
+
   attr_accessor :name, :room
 
   def ready
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Minesweeper::Application.routes.draw do
+  devise_for :users
+
 	root :to => 'index#index'
 
   resources :index, only: [:index]
